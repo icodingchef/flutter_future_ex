4f9fd3f1ef0e4f89ebe546879647f8861b11a4a9
icodingchef <icodingchef@gmail.com>
2020-08-24 22:34:24 +0900

    Initial commit

---
+++ futurePractice.dart
@@ -0,0 +1,32 @@
+void main() async {
+  methodA();
+  await methodB();
+  await methodC('main');
+  methodD();
+}
+
+methodA(){
+  print('A');
+}
+
+methodB() async {
+  print('B start');
+  await methodC('B');
+  print('B end');
+}
+
+methodC(String from) async {
+  print('C start from $from');
+
+  Future((){
+    print('C running Future from $from');
+  }).then((_){
+    print('C end of Future from $from');
+  });
+
+  print('C end from $from');
+}
+
+methodD(){
+  print('D');
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String result = 'no data found';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Future test'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  futureTest();
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Future test',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                result,
+                style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+              ),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+              ),
+              FutureBuilder(
+                  future: myFuture(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Text(
+                        snapshot.data,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blue,
+                        ),
+                      );
+                    }
+                    return CircularProgressIndicator();
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> futureTest() async {
+    await Future.delayed(Duration(seconds: 3)).then((value) {
+      print('Here comes second');
+
+      setState(() {
+        this.result = 'The data is fetched';
+        print(result);
+        print('Here comes third');
+      });
+    });
+    print('Here comes first');
+    print('Here is the last one');
+  }
+
+  Future<String> myFuture() async {
+    await Future.delayed(Duration(seconds: 2));
+    return 'another Future completed';
+  }
+}
